272a190917b5756092c726f7510f742fe65007a2
JonEgor <114680645+JonEgor@users.noreply.github.com>
2022-10-23 16:47:26 +0300

    Perebor

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    
+        int[] numbers = { 4, 1, -13, 83, 31, 58, 1, 9, 16, 72 };
+        int temp;
+for (int i = 0; i < numbers.Length; i++) {
+    for (int j = 0; j < numbers.Length - 1 - i; j++) {
+        if (numbers[j] > numbers[j + 1]) {
+            temp = numbers[j];
+            numbers[j] = numbers[j + 1];
+            numbers[j + 1] = temp;
+
+        }
+    }
+}
+for (int i=0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
