eaa6167069bd8fc56844632c8b55491a043738e3
JonEgor <114680645+JonEgor@users.noreply.github.com>
2022-10-23 15:34:32 +0300

    init commit

--- Massiv-2/Program.cs
+++ Massiv-2/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Massiv2x
+{
+    /* 
+     * вывод двумерного массива 
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = new int[,]
+          {
+                 {2,45,12,51,51},
+                 {6,7,8,3,1},
+                 {1,5,65,78,13},
+                 {6,3,151,5,65}
+          };
+
+            int height = myArray.GetLength(0); //высота таблички 
+            int width = myArray.GetLength(1); // кол. ( ширина ) инф 2 изммерения
+
+            for (int y = 0; y < height; y++) // внешний цикл 
+            {
+                for (int x = 0; x < width; x++) // внутрений цикл
+                {
+                    Console.Write(myArray[y, x] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+
+
+
+    }
+}
